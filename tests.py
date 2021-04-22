a598daeaf34e8b12b1fbd5cb0fb3daf5974f5212
brynblack <49110391+brynblack@users.noreply.github.com>
2021-04-22 18:14:07 +1000

    Trying to work out testing methodology

--- tests.py
+++ tests.py
@@ -1,2 +1,21 @@
-def test_equals():
-    assert 1 == 1
+import discord
+
+logging_channel_id = 833184929122484254
+
+intents = discord.Intents.default()
+intents.bans = True
+intents.invites = True
+intents.members = True
+intents.guild_messages = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    async def test_variables():
+        print(await client.fetch_channel(logging_channel_id))
+    await test_variables()
+
+
+client.run('NDMwNjA3NzQ0MTg2NDQ5OTIw.WsMYtg.NCUFIgQn3NtKgFa4IQ2zIYJuH8Y')
